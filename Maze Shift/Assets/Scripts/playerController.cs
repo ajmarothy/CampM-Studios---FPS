41b8308a6d438db585ff8355ad339c04bb248689
Maxwell Kelly <mpkelly@student.fullsail.edu>
2024-10-05 16:18:12 -0400

    playercontroller & cameracontroller
    added player and camera script for basic walk and look around movement

--- Maze Shift/Assets/Scripts/playerController.cs
+++ Maze Shift/Assets/Scripts/playerController.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
-
     [SerializeField] int speed;
-    
-    Vector3 playerVel;
     Vector3 moveDir;
 
     // Start is called before the first frame update
@@ -20,12 +17,10 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement();
-    }
+        //moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //transform.position += moveDir * speed * Time.deltaTime;
 
-    public void movement()
-    {
         moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
-        controller.Move(moveDir * speed* Time.deltaTime);
+        controller.Move(moveDir * speed * Time.deltaTime);
     }
 }
