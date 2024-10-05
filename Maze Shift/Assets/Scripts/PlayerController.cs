b8a6bcd064d549ca5819b5eae51096604e77b1f0
Maxwell Kelly <mpkelly@student.fullsail.edu>
2024-10-05 19:40:32 -0400

    added sprint and jump abilities to player

--- Maze Shift/Assets/Scripts/PlayerController.cs
+++ Maze Shift/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
-    [SerializeField] int speed;
+
+
+    [SerializeField] float speed;
+    [SerializeField] float sprintMod;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int gravity;
+    [SerializeField] int jumpMax;
+    
+    
+    int jumpCount;
+
+
     Vector3 moveDir;
+    Vector3 playerVel;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +29,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        //transform.position += moveDir * speed * Time.deltaTime;
+        movement();
+        sprint();
+    }
+
+
+    void movement()
+    {
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
 
         moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
         controller.Move(moveDir * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+        }
+        playerVel.y -= gravity * Time.deltaTime;
+        controller.Move(playerVel * Time.deltaTime);
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
     }
 }
