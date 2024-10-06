af2bdce113eedf06eaf33fcac46adb2d98b0646a
Carin Macario <CMacario@student.fullsail.edu>
2024-10-06 01:39:27 -0500

    Button Functions and working buttons

--- Maze Shift/Assets/Scripts/Button Functions.cs
+++ Maze Shift/Assets/Scripts/Button Functions.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonFunctions : MonoBehaviour
 {
    
+    public void resume()
+    {
+        GameManager.instance.unpause();
+    }
+
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.instance.unpause();
+    }
+
+
+
+    public void quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+            Application.Quit();
+
+        #endif
+    }
 }
