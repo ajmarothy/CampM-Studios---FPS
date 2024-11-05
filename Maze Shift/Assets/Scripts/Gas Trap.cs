854540b25a0bb7e80868a1afea1261625340f1bf
Carin Macario <CMacario@student.fullsail.edu>
2024-11-05 06:23:30 -0600

    Gas trap fixed

--- Maze Shift/Assets/Scripts/Gas Trap.cs
+++ Maze Shift/Assets/Scripts/Gas Trap.cs
@@ -6,28 +6,36 @@ public class GasTrap : MonoBehaviour
 {
     [SerializeField] int damage;
 
+    private float gasTime;
+
     
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
+        gasTime += Time.deltaTime;
+
         IDamage dmg = other.GetComponent<IDamage>();
 
         if (dmg != null )
         {
-            dmg.takeDamage(damage);
+            if (gasTime > 1)
+            {
+                dmg.takeDamage(damage);
+                gasTime = 0;
+            }
         }
     }
 
 
 
-    private void OnTriggerExit(Collider other)
-    {
-        IDamage dmg = other.GetComponent<IDamage>();
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    IDamage dmg = other.GetComponent<IDamage>();
 
-        if (dmg != null)
-        {
-            dmg.takeDamage(damage);
-        }
-    }
+    //    if (dmg != null)
+    //    {
+    //        dmg.takeDamage(damage);
+    //    }
+    //}
 
 }
