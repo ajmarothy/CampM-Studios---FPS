e001d911b2ddb6c6851e6802684934e0af536c29
Carin Macario <CMacario@student.fullsail.edu>
2024-11-13 06:19:36 -0600

    Spike Initial

--- Maze Shift/Assets/Scripts/SpikeTrap Animation.cs
+++ Maze Shift/Assets/Scripts/SpikeTrap Animation.cs
@@ -8,9 +8,14 @@ public class SpikeTrapAnim : MonoBehaviour
 
    // public static SpikeTrapAnim instance;
 
-    public Animator spikeAnim; 
+    public Animator spikeAnim;
 
-    
+
+    [SerializeField] AudioSource spikeSource;
+    [SerializeField] AudioClip[] spikeSounds;
+    [SerializeField] float spikeVol;
+
+
     void Awake()
     {
         spikeAnim = GetComponent<Animator>();
@@ -23,8 +28,10 @@ public class SpikeTrapAnim : MonoBehaviour
     {
         
         spikeAnim.SetTrigger("open");
+        spikeSource.PlayOneShot(spikeSounds[Random.Range(0, spikeSounds.Length)], spikeVol);
         yield return new WaitForSeconds(2.5f);
         spikeAnim.SetTrigger("close");
+        spikeSource.PlayOneShot(spikeSounds[Random.Range(0, spikeSounds.Length)], spikeVol);
         yield return new WaitForSeconds(2.5f);
         StartCoroutine(WorkingTrap());
 
