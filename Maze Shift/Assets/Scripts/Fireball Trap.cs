a6ab3d920e01b13a81fad7b7c939b77fbe0db648
Carin Macario <CMacario@student.fullsail.edu>
2024-11-15 03:53:37 -0600

    Fireball trap makes sound

--- Maze Shift/Assets/Scripts/Fireball Trap.cs
+++ Maze Shift/Assets/Scripts/Fireball Trap.cs
@@ -11,6 +11,10 @@ public class FireballTrap : MonoBehaviour
 
     public GameObject fireball;
 
+    [SerializeField] AudioSource cannonSource;
+    [SerializeField] AudioClip[] cannonFire;
+    [SerializeField] float cannonVol;
+
 
     // Start is called before the first frame update
     void Start()
@@ -39,8 +43,10 @@ public class FireballTrap : MonoBehaviour
 
     IEnumerator shootFireball()
     {
+        cannonSource.PlayOneShot(cannonFire[Random.Range(0, cannonFire.Length)], cannonVol);
         GameObject projectile1 = Instantiate(fireball, fire1.position, fire1.rotation);                //I want it to be this but actually work Damage.damageType.fireBall, fire1);
         yield return new WaitForSeconds(2);
+        cannonSource.PlayOneShot(cannonFire[Random.Range(0, cannonFire.Length)], cannonVol);
         GameObject projectile2 = Instantiate(fireball, fire2.position, fire2.rotation);
         yield return new WaitForSeconds(2);
     }
